5389561b115400fdb63c04d86941fb70e1bbf286
Björn Kimminich <bjoern.kimminich@gmx.de>
2015-10-13 19:51:19 +0200

    added simple student evaluation
    (parses YAML and prints to console, fails on broken YAML format)

--- src/StudentenAuswertung.java
+++ src/StudentenAuswertung.java
@@ -1,20 +1,41 @@
 import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Map;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class StudentenAuswertung {
 
   private static Yaml yaml = new Yaml();
 
-  public static void main(String... args) throws IOException {
-    InputStream bkimminich = new FileInputStream(new File("zenturien/i99a/bkimminich.yml"));
-    Map<String, Object> dozent = (Map<String, Object>) yaml.load(bkimminich);
-    System.out.println(dozent);
+  public static void main(String... args) {
+    try {
+      Path startPath = Paths.get("zenturien");
+      Files.walkFileTree(startPath, new SimpleFileVisitor<Path>() {
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir,
+                                                 BasicFileAttributes attrs) {
+          return FileVisitResult.CONTINUE;
+        }
 
-    // TODO Iteration über alle Zenturienordner und Ausgabe aller jeweils enthaltenen Studenten
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws FileNotFoundException {
+          InputStream stream = new FileInputStream(file.toFile());
+          Map<String, Object> student = (Map<String, Object>) yaml.load(stream);
+
+          System.out.println(file.toString() + " -> " + student);
+          return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException e) {
+          return FileVisitResult.CONTINUE;
+        }
+      });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     // TODO Generieren von Statistiken über Vorkenntnisse je Zenturie
     // TODO Generieren einer HTML-Seite mit allen Daten inkl. Fotos als <img>
   }
